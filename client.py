1036d03a28a497152e4478770ee735ebe18a1e23
Kyle Tseng <kyletseng1699@gmail.com>
2022-07-11 18:37:16 -0700

    added comments

--- client.py
+++ client.py
@@ -7,9 +7,12 @@ PORT = 7070  # The port used by the server
 
 def tcp_client():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # Establish connection with assigned host and port
         s.connect((HOST, PORT))
         start = time.time()
+        # Send data to the server
         s.sendall(b"Hello, world")
+        # Receive reply from the server
         data = s.recv(1024)
         end =  time.time()
 
@@ -23,7 +26,9 @@ def udp_client():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         for i in range(10):
             start = time.time()
+            # Send str to the assigned host and port
             s.sendto(str.encode("Hello world"), (HOST, PORT))
+            # Receives reply from the server
             data = s.recvfrom(1024)
             if data:
                 end =  time.time()
@@ -36,3 +41,5 @@ def udp_client():
 
 # udp_client()
 tcp_client()
+
+# Choose with client to run by commenting and uncommenting udp_client() and tcp_client()
--- server.py
+++ server.py
@@ -4,11 +4,15 @@ HOST = "127.0.0.1"
 PORT = 7070
 
 
+# UDP server
+# No connection required and no closing socket required
 def udp_server():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        # Bind the address and port
         s.bind((HOST, PORT))
         
         while True:
+            # Returns a bytes object read from an UDP socket and the address of the client socket as a tuple
             bytesAddressPair = s.recvfrom(1024)
             if not bytesAddressPair:
                 break
